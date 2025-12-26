6e73dd4b9b114cd1526be5b6195ed7fe3bee5d68
Vedant <vedantarole@rediffmail.com>
2025-12-26 16:33:15 +0530

    final released

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t 944101542155.dkr.ecr.us-east-1.amazonaws.com/vedant7669/prod_infra/base-image:10.1.1 .
+docker build -t 944101542155.dkr.ecr.us-east-1.amazonaws.com/vedant7669/prod_infra:base-image-10.1.0 .
 
 sleep 2
 
-docker push 944101542155.dkr.ecr.us-east-1.amazonaws.com/vedant7669/prod_infra/base-image:10.1.1
+docker push 944101542155.dkr.ecr.us-east-1.amazonaws.com/vedant7669/prod_infra:base-image-10.1.0
