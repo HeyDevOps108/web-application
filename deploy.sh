216a1a881699baea1293c4d5067e4b9528931fd2
Vedant <vedantarole@rediffmail.com>
2025-12-26 16:53:06 +0530

    final released

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,23 @@ if docker ps -a --format '{{.Names}}' | grep -w ${CONTAINER_NAME}; then
   docker rm -f ${CONTAINER_NAME}
 fi
 
+#increment the port by 1
+LAST_PORT=$(docker ps --format '{{.Ports}}' \
+ | grep -oP '0\.0\.0\.0:\K[0-9]+' \
+ | sort -n | tail -1)
+
+NEXT_PORT=$((LAST_PORT + 1))
+
+#sleep time
+sleep 2
+
 # Run new container
 docker run -d \
   --name ${CONTAINER_NAME} \
   --restart unless-stopped \
   --memory="512m" \
   --cpus="1.0" \
-  -p ${PORT}:80 \
+  -p ${NEXT_PORT}:80 \
   ${IMAGE_ABSOLUTE_PATH}
 
 echo "Deployment completed on $(hostname)"
