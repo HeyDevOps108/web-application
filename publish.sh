32605cc91b9b427260ad6a4b1be47d19ce800b60
Vedant <vedantarole@rediffmail.com>
2026-01-02 22:14:49 +0530

    k8s migration

--- publish.sh
+++ publish.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
   
-source common.env
+source ./common.env
 
 set -ex
 
-# replace the actual values in the dep yaml
+# replace the actual values in the dep yml
 sed \
   -i -e "s|__DEPLOYMENT_NAME__|${DEPLOYMENT_NAME}|g" \
-   artifacts/obcs-service.dep.yaml
+   artifacts/obcs-service.dep.yml
 
 
-# replace the actual values in the svc yaml
+# replace the actual values in the svc yml
 sed \
   -i -e "s|__DEPLOYMENT_NAME__|${DEPLOYMENT_NAME}|g" \
-   artifacts/obcs-service.svc.yaml
+   artifacts/obcs-service.svc.yml
 
 sed \
   -i -e "s|__DEPLOYMENT_NAME__|${DEPLOYMENT_NAME}|g" \
@@ -24,7 +24,7 @@ sed \
   -i -e "s|__MEM_LIMIT__|${MEM_LIMIT}|g" \
   -i -e "s|__CPU_REQ__|${CPU_REQ}|g" \
   -i -e "s|__MEM_REQ__|${MEM_REQ}|g" \
-   artifacts/obcs-service.svc.yaml
+   artifacts/obcs-service.dep.yml
 
 #list the final image
 echo "Pushing image: ${IMAGE_ABSOLUTE_PATH}"
